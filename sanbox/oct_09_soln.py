3d811692a49f27b96a80a95560f36bf53c986bcb
Goodnews <goodnewsrx@gmail.com>
2024-10-11 20:54:00 +0100

    assignment

---
+++ sanbox/oct_09_soln.py
@@ -0,0 +1,9 @@
+first = Goodnews
+last_name = Pius
+fav_num = 5
+int * int = int
+"""
+o mathematical operations can't be done between an integer and 
+a string asides multiplication. Hence, addition of an integer
+to a string is not possible
+"""
