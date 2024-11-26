f459db63f9d441216c6e1fcf2de201b30bd688e4
Goodnews <goodnewsrx@gmail.com>
2024-11-26 11:17:22 +0100

    first assignment

---
+++ sanbox/nov_19_soln.py
@@ -0,0 +1,69 @@
+def about_you(name,age,hobby):
+    new_name = name.title()
+    new_age = age**5
+    new_hobby = hobby.upper()
+    return f"Hi {new_name}, you are {new_age} years old. You love {new_hobby}. welcome"
+print(about_you("goodnews",3,"reading"))
+"""
+
+"""
+def great_nums(val1, val2):
+    first_var = 1 * 6
+    second_var = 2 % 2
+    third_var = first_var + second_var
+    return third_var
+print(great_nums(5,8))
+"""
+
+
+""" 
+
+ocean = "pacific ocean"
+ocean[6] #will give c
+ocean[8] #will give o
+ocean[12] #will give n
+ocean[25] #will give index error
+
+mountain = "mount kilimangaro"
+mountain[6] #will give k
+mountain[8] #will give l
+mountain[12] #will give n
+mountain[25] #will give index error
+
+place = "national park"
+place[6] #will give a
+place[8] #will give -
+place[12] #will give k
+place[25] #will give index error
+
+word = "unscrupulous"
+word[8] #will give l
+
+game = "carom billiards"
+game[13] #will give d
+
+plant = "ashwagandha"
+plant[7] #will give n
+"""
+"""
+book = "encyclopedia"
+drug = "acetaminophen"
+food = "usung ikpong ye afia efere ebot"
+
+len(book) #will give 12
+len(drug) #will give 13
+len(food) #will give 31
+
+book[0] #will give e
+book[2] #will give c
+book[8] #will give e
+
+drug[0] #will give a
+drug[2] #will give e
+drug[8] #will give o
+
+food[0] #will give u
+food[2] #will give u
+food[8] #will give p
+
+    
