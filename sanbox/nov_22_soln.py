ef670d72aff6842708cbd00a9442416ea0f1a9d2
Goodnews <goodnewsrx@gmail.com>
2024-11-26 14:02:22 +0100

    my second assignment

---
+++ sanbox/nov_22_soln.py
@@ -0,0 +1,19 @@
+def last_letter(day):
+    return day[-1]
+day = "tuesday"
+print (last_letter(day))
+
+"""
+"""
+def word(sermon):
+    return sermon[-2]
+sermon = "salvation"
+len(sermon) == 9
+print (word(sermon))
+"""
+"""
+def animal(dinosaur):
+    return dinosaur[len(dinosaur) - 3]
+dinosaur = "giganotosaurus"
+len(dinosaur) == 14
+print (animal(dinosaur))
